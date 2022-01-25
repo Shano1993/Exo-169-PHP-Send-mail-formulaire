9c2969b99c4f3c3ce03de98837f3888fd45789d3
Shano1993 <stefan.hanotiau@gmail.com>
2022-01-25 14:57:43 +0100

    php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Exo 169</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <form action="">
+        <div>
+            <label for="id-mail">
+                <input type="email" id="id-mail" name="mail" placeholder="Email">
+            </label>
+        </div>
+        <div>
+            <label for="id-message">
+                <textarea name="message" id="id-message" cols="30" rows="10"></textarea>
+            </label>
+        </div>
+        <input type="submit" name="submit" id="id-submit">
+    </form>
+</body>
+</html>
